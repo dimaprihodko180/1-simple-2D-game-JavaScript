7398528ac62117c3b92fb7a2acf9fc5d99d5496c
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-12-07 15:31:08 +0300

    Создан класс Entities

---
+++ app/canvas/Entities.js
@@ -0,0 +1,25 @@
+export class Entities {
+  #ctx
+  #canvas
+  #canvasId
+  #gCanvasWidth
+  #gCanvasHeigh
+  #playerImage
+
+  constructor(canvasId = "canvas-1") {
+    this.#canvasId = canvasId
+    this.#canvas = document.getElementById(this.#canvasId)
+    this.#ctx = this.#canvas.getContext("2d")
+    this.#gCanvasWidth = this.#canvas.width = 600
+    this.#gCanvasHeigh = this.#canvas.height = 600
+    this.#playerImage = new Image()
+    this.#playerImage.src = "shadow_dog.png"
+    this.animate = this.animate.bind(this)
+  }
+
+  animate() {
+    this.#ctx.clearRect(0, 0, this.#gCanvasWidth, this.#gCanvasHeigh)
+    this.#ctx.fillRect(50, 50, 100, 100)
+    requestAnimationFrame(this.animate)
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,4 @@
+import { Entities } from "./canvas/entities.js"
+
+const entities = new Entities()
+entities.animate()
