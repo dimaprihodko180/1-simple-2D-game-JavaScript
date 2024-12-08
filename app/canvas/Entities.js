a477fbf01e1d7987f365ecd14c5b44f61532fc4b
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-12-08 14:11:27 +0300

    ...

--- app/canvas/Entities.js
+++ app/canvas/Entities.js
@@ -24,7 +24,6 @@ export class Entities {
     { name: "getHit", frames: 4 },
   ]
   #statePosition = this.#animationStates[0].name
-
   constructor(canvasId = "canvas-1") {
     this.#canvasId = canvasId
     this.#canvas = document.getElementById(this.#canvasId)
@@ -37,11 +36,8 @@ export class Entities {
     this.#playerImage = new Image()
     this.#playerImage.src = "/img/shadow_dog.png"
     this.animate = this.animate.bind(this)
-
-    this.#initAnimations() // Инициализация анимаций
+    this.#initAnimations()
   }
-
-  // Инициализация анимаций
   #initAnimations() {
     this.#animationStates.forEach((state, index) => {
       let frame = { loc: [] }
@@ -50,42 +46,34 @@ export class Entities {
         let positionY = index * this.#spriteHeigh
         frame.loc.push({ x: positionX, y: positionY })
       }
-      this.#spriteAnimations[state.name] = frame // Исправлено: "frames" на "frame"
+      this.#spriteAnimations[state.name] = frame
     })
   }
-
   #toAnimate() {
     this.#ctx.clearRect(0, 0, this.#gCanvasWidth, this.#gCanvasHeigh)
-
-    // Проверка существования "jump" анимации
     if (!this.#spriteAnimations[this.#statePosition]) {
       console.error("Animation 'jump' not initialized.")
       return
     }
-
     let position =
       Math.floor(this.#gameFrame / this.#staggerFrame) %
-      this.#spriteAnimations[this.#statePosition].loc.length // Исправлено: "lenght" на "length"
-
-    let frameX = this.#spriteAnimations[this.#statePosition].loc[position].x // Исправлено: использован "x"
-    let frameY = this.#spriteAnimations[this.#statePosition].loc[position].y // Исправлено: использован "y"
-
+      this.#spriteAnimations[this.#statePosition].loc.length
+    let frameX = this.#spriteAnimations[this.#statePosition].loc[position].x
+    let frameY = this.#spriteAnimations[this.#statePosition].loc[position].y
     this.#ctx.drawImage(
       this.#playerImage,
-      frameX, // Источник: X
-      frameY, // Источник: Y
-      this.#spriteWidth, // Ширина кадра
-      this.#spriteHeigh, // Высота кадра
-      0, // Координата X на холсте
-      0, // Координата Y на холсте
-      this.#spriteWidth, // Ширина на холсте
-      this.#spriteHeigh // Высота на холсте
+      frameX,
+      frameY,
+      this.#spriteWidth,
+      this.#spriteHeigh,
+      0,
+      0,
+      this.#spriteWidth,
+      this.#spriteHeigh
     )
-
     this.#gameFrame++
     requestAnimationFrame(this.animate)
   }
-
   animate() {
     this.#toAnimate()
   }
