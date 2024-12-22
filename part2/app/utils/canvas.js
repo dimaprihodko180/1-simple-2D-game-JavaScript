52501551840658bc6b9b2bc86ecea025f82ca88f
dimbo1324 <dim4dmi7rij@yandex.ru>
2024-12-22 19:57:36 +0300

    Сделать изображения ботов в виде порций

--- part2/app/utils/canvas.js
+++ part2/app/utils/canvas.js
@@ -1,4 +1,4 @@
-export const canvas = document.getElementById("canvas1")
+export const canvas = document.getElementById("canvas-1")
 export const ctx = canvas.getContext("2d")
 export const CANVAS_WIDTH = (canvas.width = 800)
 export const CANVAS_HEIGHT = (canvas.height = 700)
