d8b095490154905482c0121dac5dfe8102e2576d
dimbo1324 <dim4dmi7rij@yandex.ru>
2025-01-04 16:15:29 +0300

    Проблема дублирования анимации!

--- part4/app/index.js
+++ part4/app/index.js
@@ -1,27 +1,44 @@
-/**@type {HTMLCanvasElement} */
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas-1");
 const ctx = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 700;
+
 let explosions = [];
-let canvasPosition = canvas.getBoundingClientRect();
+const canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
   constructor(x, y) {
     this.spriteWidth = 200;
     this.spriteHeight = 179;
-    this.width = 0.7 * this.spriteWidth;
-    this.height = 0.7 * this.spriteHeight;
+    this.scale = 0.7;
+    this.width = this.spriteWidth * this.scale;
+    this.height = this.spriteHeight * this.scale;
     this.x = x;
     this.y = y;
-    this.image = new Image();
-    this.image.src = "/part4/img/boom.png";
+    this.image = Explosion.loadImage("/part4/img/boom.png");
     this.frame = 0;
     this.timer = 0;
-    this.angle = Math.random() * 6.2;
+    this.angle = Math.random() * Math.PI * 2;
+    this.sound = Explosion.loadSound("/part4/audio/boom.wav");
+  }
+
+  static loadImage(src) {
+    const img = new Image();
+    img.src = src;
+    return img;
+  }
+
+  static loadSound(src) {
+    const audio = new Audio();
+    audio.src = src;
+    return audio;
   }
 
   update() {
+    if (this.frame === 0) {
+      this.sound.play();
+    }
     this.timer++;
     if (this.timer % 10 === 0) {
       this.frame++;
@@ -38,35 +55,35 @@ class Explosion {
       0,
       this.spriteWidth,
       this.spriteHeight,
-      0 - this.width / 2,
-      0 - this.height / 2,
+      -this.width / 2,
+      -this.height / 2,
       this.width,
       this.height
     );
     ctx.restore();
   }
+
+  isAnimationComplete() {
+    return this.frame > 5;
+  }
 }
 
-window.addEventListener("click", (e) => {
-  createAnimation(e);
-});
-
 function createAnimation(e) {
-  let positionX = e.x - canvasPosition.left;
-  let positionY = e.y - canvasPosition.top;
+  const positionX = e.x - canvasPosition.left;
+  const positionY = e.y - canvasPosition.top;
   explosions.push(new Explosion(positionX, positionY));
 }
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < explosions.length; i++) {
-    explosions[i].update();
-    explosions[i].draw();
-    if (explosions[i].frame > 5) {
-      explosions.splice(i, 1);
-      i--;
-    }
-  }
+  explosions = explosions.filter((explosion) => {
+    explosion.update();
+    explosion.draw();
+    return !explosion.isAnimationComplete();
+  });
   requestAnimationFrame(animate);
 }
+
+window.addEventListener("click", createAnimation);
+
 animate();
