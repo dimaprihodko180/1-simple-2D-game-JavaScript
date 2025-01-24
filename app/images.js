9b9d12bb04ea7ea516962e5fb54cc059525be2d4
dimbo1324 <dim4dmi7rij@yandex.ru>
2025-01-24 21:57:28 +0300

    1) декомпозиция приложений

--- app/images.js
+++ app/images.js
@@ -1,35 +1,35 @@
 export const imageOfPlayer = new Image();
-imageOfPlayer.src = "../../img/player.png";
+imageOfPlayer.src = "../files/images/player.png";
 
 export const layer1 = new Image();
-layer1.src = "../img/backgroundLayers/layer-1.png";
+layer1.src = "../files/images/backgroundLayers/layer-1.png";
 
 export const layer2 = new Image();
-layer2.src = "../img/backgroundLayers/layer-2.png";
+layer2.src = "../files/images/backgroundLayers/layer-2.png";
 
 export const layer3 = new Image();
-layer3.src = "../img/backgroundLayers/layer-3.png";
+layer3.src = "../files/images/backgroundLayers/layer-3.png";
 
 export const layer4 = new Image();
-layer4.src = "../img/backgroundLayers/layer-4.png";
+layer4.src = "../files/images/backgroundLayers/layer-4.png";
 
 export const layer5 = new Image();
-layer5.src = "../img/backgroundLayers/layer-5.png";
+layer5.src = "../files/images/backgroundLayers/layer-5.png";
 
 export const enemyFly = new Image();
-enemyFly.src = "../img/enemy_fly.png";
+enemyFly.src = "../files/images/enemy_fly.png";
 
 export const enemyPlant = new Image();
-enemyPlant.src = "../img/enemy_plant.png";
+enemyPlant.src = "../files/images/enemy_plant.png";
 
 export const enemyBigSpider = new Image();
-enemyBigSpider.src = "../img/enemy_spider_big.png";
+enemyBigSpider.src = "../files/images/enemy_spider_big.png";
 
 export const fire = new Image();
-fire.src = "../img/fire.png";
+fire.src = "../files/images/fire.png";
 
 export const boom = new Image();
-boom.src = "../img/boom.png";
+boom.src = "../files/images/boom.png";
 
 export const lives = new Image();
-lives.src = "../img/lives.png";
+lives.src = "../files/images/lives.png";
